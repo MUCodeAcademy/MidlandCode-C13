544ff09d34f227918468b147d03f21ae13206883
justinluce <luce@midlandu.com>
2023-09-20 17:31:20 -0500

    adds day 3

---
+++ Module 2 - JavaScript/Week 2/Day 3 - Wednesday/Classwork/boxRemoval.js
@@ -0,0 +1,15 @@
+//! Steps:
+
+//! Grab all of the red divs
+
+//! Add a click event listener to them
+
+//! Figure out which red div was clicked
+
+//! Find a way to correspond the red div to the matching orange div
+
+//! See if the orange div is there or not
+
+//! If it is, change it to display: none;
+
+//! if it's not, change it to display: block;
